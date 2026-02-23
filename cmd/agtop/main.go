3bf77e2c7c57577c69fa70fce5c241346be3a1f4
Justin Barnett <barnett.justin.p@gmail.com>
2026-02-23 13:07:04 -0500

    fix: remove duplicate subcommand stubs

--- cmd/agtop/main.go
+++ cmd/agtop/main.go
@@ -54,11 +54,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
-func runInit(cfg *config.Config) error {
-	return fmt.Errorf("init command not yet implemented")
-}
-
-func runCleanup(cfg *config.Config, dryRun bool) error {
-	return fmt.Errorf("cleanup command not yet implemented")
-}
